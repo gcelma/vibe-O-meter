28b1ef3fe42b89a96a71108d5e9f0e84d012a5ed
gcelma <CEGE@xxxlgroup.com>
2020-02-29 10:21:36 +0100

    first stable version

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,21 +1,35 @@
-const { User } = require('../db/models');
+const { User, Poll } = require('../db/models');
 
 module.exports = {
     Query: {
-    //   user() {
-    //     return { name: "manuel" };
-    //   }
-    getUsers: async () => await User.find({}).exec()
+    getUsers: async () => await User.find({}).exec(),
+    getPolls: async () => await Poll.find({}).exec(),
+    // getUserPolls: async (_, { userId }) => await User.findById(userId).populate("polls").exec(),
+    getUserPolls: async (_, { userId }) => {
+        let response = await User.findById(userId).lean();
+        let polls = response.polls.map(async poll => await Poll.findById(poll));
+        return polls;
+        }
     },
     Mutation: {
-        // createUser(_, { input }) {
-        //     console.log(input);
-        //   return input;
-        // }
         createUser: async(_, { input }) => {
             try {
                 let response = await User.create(input);
                 return response;
+            } catch(e) {
+                return e.message;
+            }
+        },
+        createPoll: async(_, { input }) => {
+            try {
+                return await Poll.create(input).then(poll => {
+                    User.findById(input.owner)
+                        .then(user => {
+                            user.polls.push(poll._id);
+                            return user.save();
+                        });
+                    return poll;
+                });
             } catch(e) {
                 return e.message;
             }
--- src/schema.js
+++ src/schema.js
@@ -1,30 +1,48 @@
 const { gql } = require('apollo-server')
 
 const typeDefs = gql`
-enum UserType {
-  ADMIN
-  REGULAR
-}
 
 type User {
   id: ID!
   username: String!
   email: String!
-  type: UserType!
+  password: String!
+  polls: [ID]
 }
 
 input NewUserInput {
   username: String!
   email: String!
-  type: UserType!
+  password: String!
+}
+
+type Poll {
+  id: ID!
+  name: String!
+  date: String!
+  owner: ID!
+  good: Int
+  mid: Int
+  bad: Int
+} 
+
+input NewPollInput {
+  name: String!
+  owner: ID!
+  good: Int
+  mid: Int
+  bad: Int
 }
 
 type Query {
   getUsers: [User]
+  getPolls: [Poll]
+  getUserPolls(userId: ID!): [Poll]
 }
 
 type Mutation {
   createUser(input: NewUserInput!): User!
+  createPoll(input: NewPollInput!): Poll!
 }
 `;
 
